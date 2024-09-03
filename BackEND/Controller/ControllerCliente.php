d83c1486b5e43ca55de4a938cdca56097ea0b76c
emiigonzalezzz <168480956+emiigonzalezzz@users.noreply.github.com>
2024-09-03 16:22:32 -0300

    .

--- BackEND/Controller/ControllerCliente.php
+++ BackEND/Controller/ControllerCliente.php
@@ -2,16 +2,37 @@
 
 require_once '../Model/ClienteDAO.php';
 
+
 $function = $_GET['function'];
 
-switch ($function){
-    case "Obtener":
-        ObtenerCliente();
-    break;
+switch ($function) {
+    case "LoginUsuario":
+        LoginUsuario();
+        break;
+    case "RegisterUsuario":
+        RegisterUsuario();
+        break;
 }
 
-function ObtenerCliente(){
-    $resultado = (new Cliente())->ObtenerClienteDAO();
+function RegisterUsuario(){
+    
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $contraseña = $_POST['contraseña'];
+
+    // Hashing de la contraseña
+
+    $contraseñaHash = password_hash($contraseña, PASSWORD_BCRYPT);
+
+    $resultado = (new Usuario())->RegisterUsuarioModel($usuario, $email, $contraseñaHash);
     echo json_encode($resultado);
+}
+
+function LoginUsuario(){
+    $usuario = $_POST['usuario'];
+    $contraseña = $_POST['contraseña'];
+    $resultado = (new Usuario())->LoginUsuarioModel($usuario, $contraseña);
+    echo json_encode($resultado);
+
 }
 ?>
--- BackEND/Model/ClienteDAO.php
+++ BackEND/Model/ClienteDAO.php
@@ -3,14 +3,70 @@
 require_once "../Connection/Connection.php";
 
 //Clase Cliente que contiene un método ObtenerClienteModelo 
-class Cliente {
-//Funcion ObtenerClienteModelo que 
-    public function ObtenerClienteDAO(){
+
+class Usuario { 
+
+    function LoginUsuarioModel($usuario, $contraseña){
         $connection = connection();
-        $sql = "SELECT * FROM cliente";
-        $respuesta = $connection->query($sql);
-        $clientes = $respuesta->fetch_all(MYSQLI_ASSOC);
-        return $clientes;
+
+        $sql = "SELECT * FROM usuario WHERE usuario = ?";
+        $stmt = $connection->prepare($sql);
+        $stmt->bind_param("s", $usuario);
+        $stmt->execute();
+        $respuesta = $stmt->get_result();
+        $resultado = $respuesta->fetch_assoc();
+
+        session_start();
+        $_SESSION['id_usuario'] = $resultado['id_usuario'];
+        return $resultado;
+
+        if ($resultado == null) {
+            return null;
+        } else {
+            // Verificar la contraseña usando password_verify()
+            if (password_verify($contraseña, $resultado['contraseña'])) {
+                $_SESSION["usuario"] = $usuario;
+                return "resultado correcto";
+            } else {
+                return "Contraseña incorrecta";
+            }
+        }
+    }
+    
+    function RegisterUsuarioModel($usuario, $email, $contraseña){
+        $connection = connection();
+
+        // Hash de la contraseña
+        $contraseñaHash = password_hash($contraseña, PASSWORD_BCRYPT);
+
+        $sql = "INSERT INTO usuario(usuario, email, contraseña) VALUES(?, ?, ?)";
+        $stmt = $connection->prepare($sql);
+        $stmt->bind_param("sss", $usuario, $email, $contraseñaHash);
+        $respuesta = $stmt->execute();
+        $stmt->close();
+
+        return $respuesta;
+    }
+
+    function ActualizarContraseñaModel($contraseña, $email){
+        $connection = connection();
+
+        // Hash de la nueva contraseña
+        $contraseñaHash = password_hash($contraseña, PASSWORD_BCRYPT);
+
+
+        $sql = "UPDATE usuario SET contraseña = ? WHERE email = ?";
+        $stmt = $connection->prepare($sql);
+        $stmt->bind_param("ss", $contraseñaHash, $email);
+        $respuesta = $stmt->execute();
+
+        // Cerrar la sentencia
+        $stmt->close();
+
+        return $respuesta;
     }
 }
+
+
+
 ?>
